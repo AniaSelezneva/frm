8d3ead47f46e8d702e34c05775619b0f7fcbcafb
Anna <bamguanat@yandex.ru>
2020-09-14 21:30:15 +0100

    Fifth commit

---
+++ build/precache-manifest.18dcc844a71233e403ab3d12d3e6d45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fbff3ed37b64d5c61c61ad6c415495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44af4a0ccc4c02c737ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4dd9fc21f6255c822309",
+    "url": "/static/js/2.1f23b820.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/static/js/2.1f23b820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44af4a0ccc4c02c737ff",
+    "url": "/static/js/main.08a707b8.chunk.js"
+  },
+  {
+    "revision": "607453c95d7dc95706f9",
+    "url": "/static/js/runtime-main.d0588bb2.js"
+  }
+]);
