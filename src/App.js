1c67af1dfeafafcefe60823b1b70e7df6d4302f8
Anna <bamguanat@yandex.ru>
2020-09-14 21:07:34 +0100

    Third commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Components
 import Signup from "./components/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route to="/" component={Home} />
         <Route to="/signup" component={Signup} />
       </Switch>
     </Router>
