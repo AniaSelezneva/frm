b6bf485c06412d677dd68aaeb8844545a300a9c8
Anna <bamguanat@yandex.ru>
2020-09-14 21:51:58 +0100

    Changed redirects

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={Signup} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     </Router>
   );
