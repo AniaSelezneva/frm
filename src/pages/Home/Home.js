063b70977535a9f860b7d205db7442fa383f0e4d
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2020-10-13 14:54:38 +0300

    changed before and after in pagination

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -21,8 +21,13 @@ function Home({ setIsLoading }) {
   const { state, dispatch } = useContext(store);
   const [path, setPath] = useState();
   const [error, setError] = useState(undefined);
+  const [ready, setReady] = useState(false);
   // Number of posts per page
   const size = 5;
+
+  window.addEventListener("load", (event) => {
+    setReady(true);
+  });
 
   // User name for any user's route
   let userName = window.location.pathname
@@ -214,77 +219,80 @@ function Home({ setIsLoading }) {
 
   return (
     <Layout>
-      <div id={homeStyles.container}>
-        <div id={homeStyles.posts_container}>
-          {/* Confirm email path */}
-          {path === "confirm" &&
-            (error === undefined ? (
-              <p className={homeStyles.posts_header}>Email confirmed</p>
-            ) : (
-              <p className="error_message">{error}</p>
-            ))}
-
-          {/* Home path */}
-          {path === "home" &&
-            // If logged in...
-            (state.loggedIn ? (
-              <NewPost />
-            ) : (
-              // If not logged in...
-              <p className={homeStyles.posts_header}>
-                <Link to="/login">
-                  <strong>login </strong>
-                </Link>
-                or
-                <Link to="/signup">
-                  <strong> signup </strong>
-                </Link>
-                to create a post
-              </p>
-            ))}
-
-          {/* Search path */}
-          {path === "search" &&
-            (state.posts.data !== undefined && state.posts.data.length > 0 ? (
-              <p className={homeStyles.posts_header}>
-                Search results for: {state.query}
-              </p>
-            ) : (
-              <p className={homeStyles.posts_header}>
-                nothing was found: {state.query}
-              </p>
-            ))}
-
-          {/* Profile path */}
-          {path === "profile" && state.loggedIn && (
-            <>
-              {state.posts.data !== undefined && state.posts.data.length > 0 ? (
-                <h2 className={homeStyles.posts_header}>My posts</h2>
+      {ready && (
+        <div id={homeStyles.container}>
+          <div id={homeStyles.posts_container}>
+            {/* Confirm email path */}
+            {path === "confirm" &&
+              (error === undefined ? (
+                <p className={homeStyles.posts_header}>Email confirmed</p>
               ) : (
-                <h2 className={homeStyles.posts_header}>
-                  You haven't posted yet
-                </h2>
-              )}
-            </>
-          )}
+                <p className="error_message">{error}</p>
+              ))}
 
-          {/* Other user's path */}
-          {path === "user" && (
-            <h2 className={homeStyles.posts_header}>
-              {/* User's name from pathname */}
-              {userName}
-            </h2>
-          )}
+            {/* Home path */}
+            {path === "home" &&
+              // If logged in...
+              (state.loggedIn ? (
+                <NewPost />
+              ) : (
+                // If not logged in...
+                <p className={homeStyles.posts_header}>
+                  <Link to="/login">
+                    <strong>login </strong>
+                  </Link>
+                  or
+                  <Link to="/signup">
+                    <strong> signup </strong>
+                  </Link>
+                  to create a post
+                </p>
+              ))}
 
-          {/* All posts */}
-          <PostsContainer path={path} />
+            {/* Search path */}
+            {path === "search" &&
+              (state.posts.data !== undefined && state.posts.data.length > 0 ? (
+                <p className={homeStyles.posts_header}>
+                  Search results for: {state.query}
+                </p>
+              ) : (
+                <p className={homeStyles.posts_header}>
+                  nothing was found: {state.query}
+                </p>
+              ))}
+
+            {/* Profile path */}
+            {path === "profile" && state.loggedIn && (
+              <>
+                {state.posts.data !== undefined &&
+                state.posts.data.length > 0 ? (
+                  <h2 className={homeStyles.posts_header}>My posts</h2>
+                ) : (
+                  <h2 className={homeStyles.posts_header}>
+                    You haven't posted yet
+                  </h2>
+                )}
+              </>
+            )}
+
+            {/* Other user's path */}
+            {path === "user" && (
+              <h2 className={homeStyles.posts_header}>
+                {/* User's name from pathname */}
+                {userName}
+              </h2>
+            )}
+
+            {/* All posts */}
+            <PostsContainer path={path} />
+          </div>
+
+          {/* Don't show user's card on the right if it's 'confirm' or 'invite' path */}
+          {path !== "confirm" && path !== "invite" && (
+            <User path={path} handle={userName} />
+          )}
         </div>
-
-        {/* Don't show user's card on the right if it's 'confirm' or 'invite' path */}
-        {path !== "confirm" && path !== "invite" && (
-          <User path={path} handle={userName} />
-        )}
-      </div>
+      )}
     </Layout>
   );
 }
