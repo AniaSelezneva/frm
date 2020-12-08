aad970b03ce48104e972dfd993873ef7e5eeacf5
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2020-12-08 03:45:13 +0300

    Experimenting with headers and inline styles

---
+++ build/precache-manifest.7e400da833081d6fdc82af4f5b0f43e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487c1f0510a9b93771f0603a179d1a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cfba146b1716b6fa660",
+    "url": "/static/css/main.a62eaec0.chunk.css"
+  },
+  {
+    "revision": "eb5675a3076b075a08f2",
+    "url": "/static/js/2.39bdb92d.chunk.js"
+  },
+  {
+    "revision": "234b9a5c76c99b4d30da7c5981a2371c",
+    "url": "/static/js/2.39bdb92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfba146b1716b6fa660",
+    "url": "/static/js/main.7b10884b.chunk.js"
+  },
+  {
+    "revision": "607453c95d7dc95706f9",
+    "url": "/static/js/runtime-main.d0588bb2.js"
+  }
+]);
